c9cd198a9f0a92e3c2ca6e97ca437aee180ffe13
G SAI KUMAR <gsaikumar2505@gmail.com>
2025-05-30 20:37:08 +0530

    Add mock data for restaurants including details like name, cuisines, and ratings

---
+++ src/utils/mockData.js
@@ -0,0 +1,136 @@
+mockData = [
+    {
+        "name": "Hai Tiffin's",
+        "cloudinaryImageId": "rdvbjt0bpk7kilwmkfv1",
+        "costForTwo": "₹150 for two",
+        "cuisines": ["South Indian"],
+        "avgRating": 4.4,
+        "veg": true,
+    },
+    {
+        "name": "Relax Tea Corner & Restaurant",
+        "cloudinaryImageId": "ugksjgtmoserdeez1gn0",
+        "costForTwo": "₹150 for two",
+        "cuisines": ["South Indian","Chinese"],
+        "avgRating": 4.3,
+        "veg": true,
+    },
+    {
+        "name": "Adithya Central",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/20/746c2a8b-61b3-4d04-81d8-738fa07dde81_185491 (1).jpg",
+        "costForTwo": "₹300 for two",
+        "cuisines": ["Biryani"],
+        "avgRating": 4.1,
+    },
+    {
+        "name": "Madina Alpha Dum Biryani Point",
+        "cloudinaryImageId": "igt3pma56k1rreac8fxv",
+        "costForTwo": "₹250 for two",
+        "cuisines": ["Biryani"],
+        "avgRating": 4.1
+    },
+    {
+        "name": "Hotel Sithara",
+        "cloudinaryImageId": "ea1zforasg7tfc9ip1ba",
+        "costForTwo": "₹150 for two",
+        "cuisines": [
+        "South Indian"
+        ],
+        "avgRating": 4.3,
+        "veg": true,
+    },
+    {
+        "name": "Akarsh Pride Multicuisine Restaurant",
+        "cloudinaryImageId": "ycqkc9ikoorpp7mjxumg",
+        "costForTwo": "₹480 for two",
+        "cuisines": [
+        "Biryani"
+        ],
+        "avgRating": 4.2,
+    },
+    {
+        "name": "Orange Grand Tiffins",
+        "cloudinaryImageId": "a9f63cc0e4cb949c4a0f1356839695d0",
+        "costForTwo": "₹170 for two",
+        "cuisines": [
+        "South Indian"
+        ],
+        "avgRating": 4.4,
+        "veg": true,
+    },
+    {
+        "name": "Gummadi Grand",
+        "cloudinaryImageId": "vakzgsmduphfeqjqbd2l",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+        "Biryani"
+        ],
+        "avgRating": 4.3,
+    },
+    {
+        "name": "Hotel N",
+        "cloudinaryImageId": "ktboqdrzcz9gwm1ot1y0",
+        "costForTwo": "₹450 for two",
+        "cuisines": [
+        "Biryani",
+        "Chinese",
+        "North Indian"
+        ],
+        "avgRating": 4.4,
+    },
+    {
+        "name": "Pride Bowl",
+        "cloudinaryImageId": "yeyamioccggiohr38wdv",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+        "Asian",
+        "Chinese"
+        ],
+        "avgRating": 4.2,
+    },
+    {
+        "name": "Devi Homely Kitchen",
+        "cloudinaryImageId": "l5ufrtb7ndy9r3e9yuu1",
+        "costForTwo": "₹370 for two",
+        "cuisines": [
+        "Biryani",
+        "South Indian"
+        ],
+        "avgRating": 4,
+    },
+    {
+        "name": " Hai Food Park",
+        "cloudinaryImageId": "btmywa5djyo1pyxo3p2r",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+        "Biryani"
+        ],
+        "avgRating": 4.3,
+    },
+    {
+    
+        "name": "Food Engineers",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/23/c3a1d51f-8acf-4026-9ebe-c147f23ffd39_596634.jpg",
+        "costForTwo": "₹280 for two",
+        "cuisines": [
+        "Biryani",
+        "Chinese"
+        ],
+        "avgRating": 4.2,
+    },
+    {
+        "name": "Ibaco",
+        "cloudinaryImageId": "duomjfwb1wppnd3jjd4a",
+        "costForTwo": "₹200 for two",
+        "cuisines": [
+        "Desserts",
+        "Sweets",
+        "Beverages"
+        ],
+        "avgRating": 4.5,
+        "veg": true,
+    }
+];
+
+
+export default mockData;
