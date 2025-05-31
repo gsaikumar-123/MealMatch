662f5a59bfc064cc65d06f5f736e76d9a945d0c6
G SAI KUMAR <gsaikumar2505@gmail.com>
2025-05-31 21:46:30 +0530

    Add routing functionality with About, Contact, and Error components

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import { createBrowserRouter,RouterProvider} from "react-router-dom";
 
 const App = () => {
   return (
@@ -14,5 +18,22 @@ const App = () => {
   );
 }
 
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element : <App/>,
+    errorElement : <Error/>
+  },
+  {
+    path:"/about",
+    element : <About/>
+  },
+  {
+    path:"/contact",
+    element : <Contact/>
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router = {appRouter}/>);
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = ()=>{
+    return(
+        <div>
+            <h1>This is the page for contacting us</h1>
+        </div>
+    )
+}
+
+export default Contact;
